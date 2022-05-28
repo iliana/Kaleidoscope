1fb5ccfa88efe18f78a35f88f8fcc20c75b12675
iliana etaoin <iliana@buttslol.net>
2022-05-27 21:42:48 -0700

    remove Focus

--- iliana/Model100/Model100.ino
+++ iliana/Model100/Model100.ino
@@ -4,7 +4,6 @@
 // See "LICENSE" for license details
 
 #include "Kaleidoscope.h"
-#include "Kaleidoscope-FocusSerial.h"
 #include "Kaleidoscope-Macros.h"
 #include "Kaleidoscope-LEDControl.h"
 #include "Kaleidoscope-LEDEffect-SolidColor.h"
@@ -12,45 +11,62 @@
 
 enum {
   MACRO_ANY,
+  MACRO_RESET_BOOTLOADER,
 };  // macros
 
 enum {
   PRIMARY,
   FUNCTION,
+  PROG_KEY,
 };  // layers
 
 // clang-format off
 
 KEYMAPS(
   [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+  (ShiftToLayer(PROG_KEY), Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Backtick,           Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+   Key_PageUp,             Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_PageDown,           Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY), Key_6, Key_7, Key_8,     Key_9,      Key_0,         ___,
+   M(MACRO_ANY), Key_6, Key_7, Key_8,     Key_9,      Key_0,         XXX,
    Key_Enter,    Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
                  Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
-   ___,          Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
+   XXX,          Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
-  [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1, Key_F2, Key_F3, Key_F4, Key_F5, ___,
-   Key_Tab,  ___,    ___,    ___,    ___,    ___,    ___,
-   Key_Home, ___,    ___,    ___,    ___,    ___,
-   Key_End,  ___,    ___,    ___,    ___,    ___,    ___,
-   ___, Key_Delete, ___, ___,
-   ___,
+  [FUNCTION] = KEYMAP_STACKED
+  (XXX,      Key_F1, Key_F2, Key_F3, Key_F4, Key_F5, XXX,
+   Key_Tab,  XXX,    XXX,    XXX,    XXX,    XXX,    XXX,
+   Key_Home, XXX,    XXX,    XXX,    XXX,    XXX,
+   Key_End,  XXX,    XXX,    XXX,    XXX,    XXX,    XXX,
+   XXX, Key_Delete, XXX, XXX,
+   XXX,
 
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
    Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   ___,                        Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-   ___, ___, ___, ___,
-   ___)
+                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  XXX,              XXX,
+   XXX,                        Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, XXX,             Key_Backslash,    Key_Pipe,
+   XXX, XXX, XXX, XXX,
+   XXX),
+
+  [PROG_KEY] = KEYMAP_STACKED
+  (XXX, XXX, XXX, XXX, XXX, XXX, XXX,
+   XXX, XXX, XXX, XXX, XXX, XXX, XXX,
+   XXX, XXX, XXX, XXX, XXX, XXX,
+   XXX, XXX, XXX, XXX, XXX, XXX, XXX,
+   XXX, XXX, XXX, XXX,
+   XXX,
+
+   XXX,                       XXX, XXX, XXX, XXX, XXX, XXX,
+   M(MACRO_RESET_BOOTLOADER), XXX, XXX, XXX, XXX, XXX, XXX,
+                              XXX, XXX, XXX, XXX, XXX, XXX,
+   XXX,                       XXX, XXX, XXX, XXX, XXX, XXX,
+   XXX, XXX, XXX, XXX,
+   XXX)
 )
 
 // clang-format on
@@ -63,6 +79,12 @@ const macro_t *macroAction(uint8_t macro_id, KeyEvent &event) {
     if (keyToggledOn(event.state)) {
       event.key.setKeyCode(Key_A.getKeyCode() + (uint8_t)(millis() % 36));
       event.key.setFlags(0);
+    }
+    break;
+  case MACRO_RESET_BOOTLOADER:
+    if (keyToggledOn(event.state)) {
+      Macros.tap(Key_Enter);
+      kaleidoscope::Runtime.device().rebootBootloader();
     }
     break;
   }
@@ -84,7 +106,6 @@ void hostPowerManagementEventHandler(kaleidoscope::plugin::HostPowerManagement::
 }
 
 KALEIDOSCOPE_INIT_PLUGINS(
-  Focus,
   LEDControl,
   LEDOff,
   solidViolet,
